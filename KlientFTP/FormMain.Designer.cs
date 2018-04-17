c9df348a22e0009e3c89998bf099627f10f12926
Madnarth <corwyn.blade@gmail.com>
2018-04-17 22:53:45 +0200

    Initial commit

---
+++ KlientFTP/FormMain.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace KlientFTP
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxServer = new System.Windows.Forms.ComboBox();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.maskedTextBoxPass = new System.Windows.Forms.MaskedTextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.textBoxLocalPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.listBoxFtpDir = new System.Windows.Forms.ListBox();
+            this.textBoxFtpPath = new System.Windows.Forms.TextBox();
+            this.buttonUpDir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStripStatusLabelServer = new System.Windows.Forms.StatusStrip();
+            this.Serwer = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolStripStatusLabelDownload = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelServer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxServer
+            // 
+            this.comboBoxServer.FormattingEnabled = true;
+            this.comboBoxServer.Location = new System.Drawing.Point(12, 24);
+            this.comboBoxServer.Name = "comboBoxServer";
+            this.comboBoxServer.Size = new System.Drawing.Size(142, 21);
+            this.comboBoxServer.TabIndex = 0;
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(54, 51);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLogin.TabIndex = 1;
+            // 
+            // maskedTextBoxPass
+            // 
+            this.maskedTextBoxPass.Location = new System.Drawing.Point(54, 75);
+            this.maskedTextBoxPass.Name = "maskedTextBoxPass";
+            this.maskedTextBoxPass.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBoxPass.TabIndex = 2;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(12, 104);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 3;
+            this.buttonConnect.Text = "Połącz";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(93, 104);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.TabIndex = 4;
+            this.buttonDisconnect.Text = "Rozłącz";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(54, 134);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(75, 23);
+            this.buttonDownload.TabIndex = 5;
+            this.buttonDownload.Text = "Pobierz";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.Location = new System.Drawing.Point(54, 163);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpload.TabIndex = 6;
+            this.buttonUpload.Text = "Wyślij";
+            this.buttonUpload.UseVisualStyleBackColor = true;
+            this.buttonUpload.Click += new System.EventHandler(this.buttonUpload_Click);
+            // 
+            // textBoxLocalPath
+            // 
+            this.textBoxLocalPath.Location = new System.Drawing.Point(12, 235);
+            this.textBoxLocalPath.Name = "textBoxLocalPath";
+            this.textBoxLocalPath.Size = new System.Drawing.Size(300, 20);
+            this.textBoxLocalPath.TabIndex = 7;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(318, 235);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 8;
+            this.buttonBrowse.Text = "...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // listBoxFtpDir
+            // 
+            this.listBoxFtpDir.FormattingEnabled = true;
+            this.listBoxFtpDir.Location = new System.Drawing.Point(174, 52);
+            this.listBoxFtpDir.Name = "listBoxFtpDir";
+            this.listBoxFtpDir.Size = new System.Drawing.Size(219, 173);
+            this.listBoxFtpDir.TabIndex = 9;
+            // 
+            // textBoxFtpPath
+            // 
+            this.textBoxFtpPath.Location = new System.Drawing.Point(174, 24);
+            this.textBoxFtpPath.Name = "textBoxFtpPath";
+            this.textBoxFtpPath.Size = new System.Drawing.Size(138, 20);
+            this.textBoxFtpPath.TabIndex = 10;
+            // 
+            // buttonUpDir
+            // 
+            this.buttonUpDir.Location = new System.Drawing.Point(318, 24);
+            this.buttonUpDir.Name = "buttonUpDir";
+            this.buttonUpDir.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpDir.TabIndex = 11;
+            this.buttonUpDir.Text = "^";
+            this.buttonUpDir.UseVisualStyleBackColor = true;
+            this.buttonUpDir.Click += new System.EventHandler(this.buttonUpDir_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // toolStripStatusLabelServer
+            // 
+            this.toolStripStatusLabelServer.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Serwer,
+            this.toolStripStatusLabelDownload});
+            this.toolStripStatusLabelServer.Location = new System.Drawing.Point(0, 269);
+            this.toolStripStatusLabelServer.Name = "toolStripStatusLabelServer";
+            this.toolStripStatusLabelServer.Size = new System.Drawing.Size(414, 22);
+            this.toolStripStatusLabelServer.TabIndex = 12;
+            this.toolStripStatusLabelServer.Text = "Serwer";
+            // 
+            // Serwer
+            // 
+            this.Serwer.Name = "Serwer";
+            this.Serwer.Size = new System.Drawing.Size(42, 17);
+            this.Serwer.Text = "Serwer";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Login:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Hasło:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Serwer FTP";
+            // 
+            // toolStripStatusLabelDownload
+            // 
+            this.toolStripStatusLabelDownload.Name = "toolStripStatusLabelDownload";
+            this.toolStripStatusLabelDownload.Size = new System.Drawing.Size(166, 17);
+            this.toolStripStatusLabelDownload.Text = "toolStripStatusLabelDownload";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 291);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStripStatusLabelServer);
+            this.Controls.Add(this.buttonUpDir);
+            this.Controls.Add(this.textBoxFtpPath);
+            this.Controls.Add(this.listBoxFtpDir);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxLocalPath);
+            this.Controls.Add(this.buttonUpload);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.maskedTextBoxPass);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.comboBoxServer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Name = "FormMain";
+            this.Text = "Klient FTP";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormMain_KeyDown);
+            this.toolStripStatusLabelServer.ResumeLayout(false);
+            this.toolStripStatusLabelServer.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxServer;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPass;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.TextBox textBoxLocalPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.ListBox listBoxFtpDir;
+        private System.Windows.Forms.TextBox textBoxFtpPath;
+        private System.Windows.Forms.Button buttonUpDir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.StatusStrip toolStripStatusLabelServer;
+        private System.Windows.Forms.ToolStripStatusLabel Serwer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelDownload;
+    }
+}
+
---
+++ KlientFTP/FtpClient.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections;
+
+namespace KlientFTP
+{
+    class FtpClient
+    {
+        #region Pola
+        private string host;
+        private string username;
+        private string password;
+        private string ftpPath;
+        private bool downloadCompleted;
+        private bool uploadCompleted;
+        #endregion
+
+        #region Własności
+        public string Host
+        {
+            get
+            {
+                return host;
+            }
+            set
+            {
+                host = value;
+            }
+        }
+        public string UserName
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+        public string FtpDirectory
+        {
+            get
+            {
+                if (ftpPath.StartsWith("ftp://"))
+                    return ftpPath;
+                else
+                    return "ftp://" + ftpPath;
+            }
+            set
+            {
+                ftpPath = value;
+            }
+        }
+        public bool DownloadCompleted
+        {
+            get
+            {
+                return downloadCompleted;
+            }
+            set
+            {
+                downloadCompleted = value;
+            }
+        }
+        public bool UploadCompleted
+        {
+            get
+            {
+                return uploadCompleted;
+            }
+            set
+            {
+                uploadCompleted = value;
+            }
+        }
+        #endregion
+
+        public FtpClient(string host, string userName, string password)
+        {
+            this.host = host;
+            this.username = userName;
+            this.password = password;
+            ftpPath = "ftp://" + this.host;
+        }
+        public FtpClient()
+        {
+            downloadCompleted = true;
+            uploadCompleted = true;
+        }
+
+        public ArrayList GetDirectories()
+        {
+            ArrayList directories = new ArrayList();
+            FtpWebRequest request;
+            try
+            {
+                request = (FtpWebRequest)WebRequest.Create(ftpPath);
+                request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+                request.Credentials = new NetworkCredential(this.username, this.password);
+                request.KeepAlive = false;
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                {
+                    Stream stream = response.GetResponseStream();
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string directory;
+                        while ((directory = reader.ReadLine()) != null)
+                        {
+                            directories.Add(directory);
+                        }
+                    }
+                }
+                return directories;
+                }
+            catch
+            {
+                throw new Exception("Błąd: Nie można nawiązać połączenia z " + host);
+            }
+        }
+
+        public ArrayList ChangeDirectory(string DirectoryName)
+        {
+            ftpPath += "/" + DirectoryName;
+            return GetDirectories();
+        }
+        public ArrayList ChangeDirectoryUp()
+        {
+            if (ftpPath != "ftp://" + host)
+            {
+                ftpPath = ftpPath.Remove(ftpPath.LastIndexOf("/"), ftpPath.Length - ftpPath.LastIndexOf("/"));
+                return GetDirectories();
+            }
+            else
+                return GetDirectories();
+        }
+
+        public void DownloadFileAsync(string ftpFileName, string localFileName)
+        {
+            WebClient client = new WebClient();
+            try
+            {
+                Uri uri = new Uri(ftpPath + "/" + ftpFileName);
+                FileInfo file = new FileInfo(localFileName);
+                if (file.Exists)
+                    throw new Exception("Błąd: Plik " + localFileName + " istnieje");
+                else
+                {
+                    client.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                    client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                    client.Credentials = new NetworkCredential(this.UserName,this.password);
+                    client.DownloadFileAsync(uri, localFileName);
+                    downloadCompleted = false;
+                }
+            }
+            catch
+            {
+                client.Dispose();
+                throw new Exception("Błąd: Pobranie pliku niemożliwe");
+            }
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.OnDownloadProgressChanged(sender, e);
+        }
+        void client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            this.OnDownloadCompleted(sender, e);
+        }
+        #region Zdarzenia
+            public delegate void DownProgressChangedEventHandler(object sender, DownloadProgressChangedEventArgs e);
+            public event DownProgressChangedEventHandler DownProgressChanged;
+            protected virtual void OnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+            {
+                if (DownProgressChanged != null) DownProgressChanged(sender, e);
+            }
+
+            public delegate void DownCompletedEventHandler(object sender, System.ComponentModel.AsyncCompletedEventArgs e);
+            public event DownCompletedEventHandler DownCompleted;
+            protected virtual void OnDownloadCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+            {
+                if (DownCompleted != null) DownCompleted(sender, e);
+            }
+
+        public delegate void UpCompletedEventHandler(object sender, UploadFileCompletedEventArgs e);
+        public event UpCompletedEventHandler UpCompleted;
+        protected virtual void OnUploadCompleted(object sender, UploadFileCompletedEventArgs e)
+        {
+            if (UpCompleted != null) UpCompleted(sender, e);
+        }
+
+        public delegate void UpProgressChangedEventHandler(object sender, UploadProgressChangedEventArgs e);
+        public event UpProgressChangedEventHandler UpProgressChanged;
+        protected virtual void OnUploadProgressChanged(object sender, UploadProgressChangedEventArgs e)
+        {
+            if (UpProgressChanged != null) UpProgressChanged(sender, e);
+        }
+        #endregion
+        public void UploadFileAsync(string FileName)
+        {
+            try
+            {
+                System.Net.Cache.RequestCachePolicy cache = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.Reload);
+                WebClient client = new WebClient();
+                FileInfo file = new FileInfo(FileName);
+                Uri uri = new Uri((FtpDirectory + '/' + file.Name).ToString());
+                client.Credentials = new NetworkCredential(this.username, this.password);
+                uploadCompleted = false;
+                if (file.Exists)
+                {
+                    client.UploadFileCompleted += new UploadFileCompletedEventHandler(client_UploadFileCompleted);
+                    client.UploadProgressChanged += new UploadProgressChangedEventHandler(client_UploadProgressChanged);
+                    client.UploadFileAsync(uri, FileName);
+                }
+            }
+            catch
+            {
+                throw new Exception("Błąd: Nie można wysłać pliku");
+            }
+        }
+        void client_UploadProgressChanged(object sender, UploadProgressChangedEventArgs e)
+        {
+            this.OnUploadProgressChanged(sender, e);
+        }
+        void client_UploadFileCompleted(object sender, UploadFileCompletedEventArgs e)
+        {
+            this.OnUploadCompleted(sender, e);
+        }
+        public string DeleteFile(string nazwa)
+        {
+            try
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpPath + "//" + nazwa);
+                request.Method = WebRequestMethods.Ftp.DeleteFile;
+                request.Credentials = new NetworkCredential(this.username, this.password);
+                request.KeepAlive = false;
+                request.UsePassive = true;
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                return response.StatusDescription;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Błąd: Nie można usunąć pliku " + nazwa + " (" + ex.Message + ")");
+            }
+        }
+    }
+}
